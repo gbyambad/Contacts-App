14524fa8d5a09325e5a5e2a6a2f7a279e9bb557e
byambadorj2 <byambadorj993@gmail.com>
2025-11-06 15:42:01 +0800

    Initial Commit

---
+++ Contacts App/Contacts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_AppApp.swift
+//  Contacts App
+//
+//  Created by Byambadorj on 2025.11.06.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contacts App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Contacts App
+//
+//  Created by Byambadorj on 2025.11.06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
